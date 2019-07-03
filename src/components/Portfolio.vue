475316a9f17ba1efcb75a16baba89305a264a9f5
NobleTuna <lyy7661@naver.com>
2019-07-03 15:17:30 +0900

    포트폴리오, 포스트 오버플로 처리

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,30 +1,60 @@
 <template>
-  <v-card>
-    <v-img :src="imgSrc" height="200px">
-    </v-img>
-    <v-card-title primary-title>
-      <div>
-        <div class="headline">{{title}}</div>
-        <span class="grey--text">{{body}}</span>
-      </div>
-    </v-card-title>
-  </v-card>
+<v-card>
+  <v-img :src="imgSrc" height="200px">
+  </v-img>
+  <v-card-title primary-title>
+    <div>
+      <div class="headline title_overflow">{{title}}</div>
+      <span class="grey--text body_overflow">
+
+        {{body}}</span>
+    </div>
+  </v-card-title>
+</v-card>
 </template>
+
+<style>
+
+.title_overflow {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  -o-text-overflow: ellipsis;
+  -ms-text-overflow: ellipsis;
+  -moz-text-overflow: ellipsis
+}
+
+.body_overflow {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+}
+</style>
 
 <script>
 export default {
-	name: 'Portfolio',
-	props: {
-		date: {type: String},
-		title: {type: String},
-		body: {type: String},
-		imgSrc: {type: String},
-	},
-	data() {
-		return {
-			//
-		}
-	}
+  name: 'Portfolio',
+  props: {
+    date: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    body: {
+      type: String
+    },
+    imgSrc: {
+      type: String
+    },
+  },
+  data() {
+    return {
+      //
+    }
+  }
 }
 </script>
-
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,37 +1,64 @@
 <template>
-  <v-layout py-4 h-100>
-    <v-flex row>
-      <div class="caption">{{formatedDate}}</div>
-      <h2 class="color-333 headline font-weight-light">{{title}}</h2>
-      <p class="mb-1 color-666 font-weight-light subheading">{{body}}</p>
-    </v-flex>
-  </v-layout>
+<v-layout py-4 h-100>
+  <v-flex row>
+    <div class="caption">{{formatedDate}}</div>
+    <h2 class="color-333 headline font-weight-light title_overflow">{{title}}</h2>
+    <p class="mb-1 color-666 font-weight-light subheading body_overflow">
+
+      {{body}}</p>
+  </v-flex>
+</v-layout>
 </template>
 
 <script>
 export default {
-	name: 'Post',
-	props: {
-		date: {type: Date},
-		title: {type: String},
-		body: {type: String}
-	},
+  name: 'Post',
+  props: {
+    date: {
+      type: Date
+    },
+    title: {
+      type: String
+    },
+    body: {
+      type: String
+    }
+  },
   computed: {
-		formatedDate() {
-			return `${this.date.getFullYear()}년 ${this.date.getMonth()}월 ${this.date.getDate()}일`
+    formatedDate() {
+      return `${this.date.getFullYear()}년 ${this.date.getMonth()}월 ${this.date.getDate()}일`
     }
   }
 }
 </script>
 <style>
-  .color-666 {
-    color: #666;
-  }
-  .color-333 {
-    color: #333;
-  }
-  .h-100 {
-    height: 100%;
-  }
-</style>
+.color-666 {
+  color: #666;
+}
 
+.color-333 {
+  color: #333;
+}
+
+.h-100 {
+  height: 100%;
+}
+
+.title_overflow {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  -o-text-overflow: ellipsis;
+  -ms-text-overflow: ellipsis;
+  -moz-text-overflow: ellipsis
+}
+
+.body_overflow {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+}
+</style>
